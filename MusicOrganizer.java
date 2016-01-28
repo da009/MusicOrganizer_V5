14aaebaf749612ea976f3be804f2b8d05e57b2f8
Daniel Pradera <Daniel Pradera>
2016-01-28 11:53:28 +0100

    Permite introducir como String el nombre de la carpeta que contiene los archivos de audio

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -22,12 +22,12 @@ public class MusicOrganizer
     /**
      * Create a MusicOrganizer
      */
-    public MusicOrganizer()
+    public MusicOrganizer(String carpetaDeAudios)
     {
         tracks = new ArrayList<Track>();
         player = new MusicPlayer();
         reader = new TrackReader();
-        readLibrary("audio");
+        readLibrary(carpetaDeAudios);
         System.out.println("Music library loaded. " + getNumberOfTracks() + " tracks.");
         System.out.println();
         playingComplete = false;
